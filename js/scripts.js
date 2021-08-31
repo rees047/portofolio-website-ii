cbe8f11fc0564bb42e8ee35f368a6fa905e67d58
Rees <40927062+rees047@users.noreply.github.com>
2021-08-31 01:32:54 -0400

    Done Portofolio

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,22 @@
+function bannerAnimation(){
+    //animation from: https://www.sliderrevolution.com/resources/css-text-animation/
+    const spans = document.querySelectorAll('.word span');
+
+    spans.forEach((span, idx) => {
+      span.addEventListener('click', (e) => {
+        e.target.classList.add('active');
+      });
+      span.addEventListener('animationend', (e) => {
+        e.target.classList.remove('active');
+      });
+      
+      // Initial animation
+      setTimeout(() => {
+        span.classList.add('active');
+      }, 750 * (idx+1))
+    });
+}
+
 function scrolltoView(){
     $('a[role]').click(function() {
         let target = $('#' + $(this).attr('target'));
@@ -10,6 +29,7 @@ function scrolltoView(){
 
 
 $( document ).ready(function() {
-   scrolltoView();
     $('#modal-content').load('modal-content.html');
+    bannerAnimation();
+    scrolltoView();
 });
